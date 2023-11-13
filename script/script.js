894472b8c03e0fae3364b45aa741bea59a2efb02
Fireshadow111 <muhammadrajah16@gmail.com>
2023-11-13 17:06:50 +0200

    adding

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function convertTemp() {
+    let fahrenheit = document.getElementById("fahrenheit").value;
+
+    if (!isNaN(fahrenheit)) {
+        let celsius = (fahrenheit - 32) * 5/9;
+
+        alert(fahrenheit + " Fahrenheit converts to " + celsius.toFixed(2) + " Celsius.");
+    } else {
+       
+        alert("Please enter a valid unit.");
+    }
+}
